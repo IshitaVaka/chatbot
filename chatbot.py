619da94f48a7836f58662d107311e2f2f52619af
Ishita Vaka <ishita.vaka@gmail.com>
2026-03-30 17:46:33 -0700

    testing

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,21 @@
 
 print("Hello world")
+
+
+x = 5
+y = 3
+print( x + y)
+
+my_list = [1, 2, 3, 4]
+for i in my_list:
+    print(i)
+
+
+
+print("3")
+
+sum = 0
+n = int(input("calculating sum from 1 to ... (give a number)"))
+for i in range (1, n + 15):
+    sum += i
+print(sum)
